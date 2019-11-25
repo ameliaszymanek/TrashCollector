2dd6b836833c1b154088a91ae08a42e3e056c26f
Amelia Szymanek <ameliaszymanek@Amelias-MacBook-Pro.local>
2019-11-25 15:05:27 -0600

    Init commit

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        public string name { get; set; }
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zipcode { get; set; }
+        public bool isSuspended { get; set; }
+        public string extraPickUpDate { get; set; }
+        public string startDate { get; set; }
+        public string endDate { get; set; }
+
+    }
+}
---
+++ TrashCollector/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {
+        public string name { get; set; }
+        public int zipcode { get; set; }
+    }
+}
