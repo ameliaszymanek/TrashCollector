320fdcf81799586c10717d1b2455045eda16a83b
Amelia Szymanek <ameliaszymanek@Amelias-MacBook-Pro.local>
2019-11-26 15:58:37 -0600

    Figured out why registering (creating a user of type employee or customer) wasn't working. Can create user of both types now. Starting Edit view for Customer.

--- TrashCollector/Controllers/CustomerController.cs
+++ TrashCollector/Controllers/CustomerController.cs
@@ -1,98 +1,89 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
-//using TrashCollector.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
-//namespace TrashCollector.Controllers
-//{
-//    public class CustomerController : Controller
-//    {
-//        private object context;
+namespace TrashCollector.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
 
-//        // GET: Customer
-//        public ActionResult Index()
-//        {
-//            return View();
-//        }
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
 
-//        // GET: Customer/Details/5
-//        public ActionResult Details(int id)
-//        {
-//            return View();
-//        }
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
 
-//        // GET: Customer/Create
-//        public ActionResult Create()
-//        {
-//            Customer customer = new Customer()
-//            {
-//                ApplicationUser = applicationUser;
-//            }
-//            //return View(customer);
-//        }
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
 
-//        // POST: Customer/Create
-//        [HttpPost]
-//        public ActionResult Create(Customer customer)
-//        {
-//            try
-//            {
-//                // TODO: Add insert logic here
-                
-//                context.Customers.Add(customer);
-//                context.SaveChanges();
-//                return RedirectToAction("Index", "Customer");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
 
-//        // GET: Customer/Edit/5
-//        public ActionResult Edit(int id)
-//        {
-//            return View();
-//        }
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
 
-//        // POST: Customer/Edit/5
-//        [HttpPost]
-//        public ActionResult Edit(int id, FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add update logic here
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
 
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
 
-//        // GET: Customer/Delete/5
-//        public ActionResult Delete(int id)
-//        {
-//            return View();
-//        }
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
 
-//        // POST: Customer/Delete/5
-//        [HttpPost]
-//        public ActionResult Delete(int id, FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add delete logic here
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
 
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-//    }
-//}
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
